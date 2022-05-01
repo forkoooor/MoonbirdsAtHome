998bd2bf33487f25b3747f03432ada40585a5973
nfttank <nfttank>
2022-05-01 15:07:10 +0200

    Updates

---
+++ precache-manifest.bda966093e734b23c4fc75d6505496ad.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3ea55ae08b79c915775",
+    "url": "/moonbirdsathome/static/css/main.f1426964.chunk.css"
+  },
+  {
+    "revision": "f3ea55ae08b79c915775",
+    "url": "/moonbirdsathome/static/js/main.f3ea55ae.chunk.js"
+  },
+  {
+    "revision": "da6c6d3cbdf5c0d57ea3",
+    "url": "/moonbirdsathome/static/css/1.ec66cbef.chunk.css"
+  },
+  {
+    "revision": "da6c6d3cbdf5c0d57ea3",
+    "url": "/moonbirdsathome/static/js/1.da6c6d3c.chunk.js"
+  },
+  {
+    "revision": "2990b4396e83f7a8dcfa",
+    "url": "/moonbirdsathome/static/js/runtime~main.2990b439.js"
+  },
+  {
+    "revision": "6c015b5346e79b091ab03dfcee6dc8a8",
+    "url": "/moonbirdsathome/static/media/banner.6c015b53.png"
+  },
+  {
+    "revision": "f0aee2141a68ec294b02966d7cfd2979",
+    "url": "/moonbirdsathome/static/media/logos.f0aee214.woff"
+  },
+  {
+    "revision": "f2bbe2bc660e6d64ae2b19656b362152",
+    "url": "/moonbirdsathome/index.html"
+  }
+];
